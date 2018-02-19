5c68add116cd471ec9e435f54b22e02b88fb4475
jburnett2 <jburnett@trinity.edu>
2018-02-19 15:31:12 -0600

    initial enemy scene kinda

---
+++ Assets/Scripts/getEm.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getEm : MonoBehaviour {
+
+    public GameObject target;
+    public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+    }
+}
