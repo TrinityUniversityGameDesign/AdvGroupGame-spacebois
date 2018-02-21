e5e2f15fb1b0a887d267a949753df4b47ce2eb1a
Nick Samoray <nicksam112@gmail.com>
2018-02-21 15:19:54 -0600

    Fixed Player Networking

---
+++ Assets/Scripts/playerNetworking.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerNetworking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (!GetComponent<PhotonView>().isMine)
+        {
+            GetComponent<PlayerMovement>().enabled = false;
+            GetComponent<EditorCameraMovement>().enabled = false;
+            transform.GetChild(0).GetComponent<Camera>().enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
