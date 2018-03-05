95e1f71e7cf5edf3fdc2e38f5a76172a83cdb837
mnewtonten <mnewton@trinity.edu>
2018-03-04 18:35:05 -0600

    GetInRadius more realistically searches for player

--- Assets/Scripts/GetInRadius.cs
+++ Assets/Scripts/GetInRadius.cs
@@ -2,61 +2,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GetInRadius : MonoBehaviour {
+public class GetInRadius : MonoBehaviour
+{
 
-	private float targetX;
-	private float targetY;
-	private float targetZ;
-	private Vector3 targetTransform;
+    private float targetX;
+    private float targetY;
+    private float targetZ;
+    private Vector3 targetTransform;
+    private Vector3 lookTransform;
 
-	public float radius; 
-	public float speed;
-	public float randomWait;
-	private float time;
+    public float radius;
+    public float speed;
+    private float time;
+    private float randomTime;
+    public float rotationSpeed;
 
-	public GameObject targetPlayer;
+    private Vector3 dummyRotate;
+    private Transform dummyTransform;
+    private Quaternion _lookRotation;
+    private Vector3 _direction;
 
-	void SetRandomTime(){
-        randomWait = Random.Range(3, 5);
-	}
+    public GameObject targetPlayer;
 
-	// Use this for initialization
-	void Start () {
-		time = 0;
-		SetRandomTime ();
-		
-		do {
-			targetX = Random.Range(targetPlayer.transform.position.x - radius, targetPlayer.transform.position.x + radius);
-			targetY = Random.Range(targetPlayer.transform.position.y - radius, targetPlayer.transform.position.y + radius);
-			targetZ = Random.Range(targetPlayer.transform.position.z - radius, targetPlayer.transform.position.z + radius);
-		} while (targetX * targetX + targetY * targetY + targetZ * targetZ > radius * radius);
+    void SetRandomTime()
+    {
+        randomTime = Random.Range(1, 5);
+    }
 
-		targetTransform = new Vector3(targetX, targetY, targetZ);
+    float differenceOfRotation(Quaternion a, Quaternion b)
+    {
+        Quaternion diff = Quaternion.Inverse(a) * b;
+        float sum = Mathf.Abs(diff.x) + Mathf.Abs(diff.y) + Mathf.Abs(diff.z);
+        return sum;
+    }
 
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+void Start()
+    {
+        //rotationSpeed = 1.75f;
+        time = 0;
+        SetRandomTime();
+        targetX = Random.Range(targetPlayer.transform.position.x - radius, targetPlayer.transform.position.x + radius);
+        targetY = Random.Range(targetPlayer.transform.position.y - radius, targetPlayer.transform.position.y + radius);
+        targetZ = Random.Range(targetPlayer.transform.position.z - radius, targetPlayer.transform.position.z + radius);
+        targetTransform = new Vector3(targetX, targetY, targetZ);
+
+        _direction = (targetTransform - transform.position).normalized;
+        _lookRotation = Quaternion.LookRotation(_direction);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
 
-		float step = speed * Time.deltaTime;
+        float step = speed * Time.deltaTime;
 
-		if (Vector3.Distance (transform.position, targetTransform) < 2) {
-			time += Time.deltaTime;
-		}
+        if (Vector3.Distance(transform.position, targetTransform) < 2.5)
+        {
+            time += Time.deltaTime;
+        }
+        if (time > randomTime)
+        {
+            
+            time = 0;
+            SetRandomTime();
+            targetX = Random.Range(targetPlayer.transform.position.x - radius, targetPlayer.transform.position.x + radius);
+            targetY = Random.Range(targetPlayer.transform.position.y - radius, targetPlayer.transform.position.y + radius);
+            targetZ = Random.Range(targetPlayer.transform.position.z - radius, targetPlayer.transform.position.z + radius);
+            targetTransform = new Vector3(targetX, targetY, targetZ);
 
-		if(time > randomWait){
-			do {
-				targetX = Random.Range (targetPlayer.transform.position.x - radius, targetPlayer.transform.position.x + radius);
-				targetY = Random.Range (targetPlayer.transform.position.y - radius, targetPlayer.transform.position.y + radius);
-				targetZ = Random.Range (targetPlayer.transform.position.z - radius, targetPlayer.transform.position.z + radius);
-			} while (targetX * targetX + targetY * targetY + targetZ * targetZ > radius * radius);
-			targetTransform = new Vector3(targetX, targetY, targetZ);
-			time = 0; 
-			SetRandomTime ();
-		} else {
-			transform.position = Vector3.MoveTowards (transform.position, targetTransform, step);
-		}
-	}
+            _direction = (targetTransform - transform.position).normalized;
+            _lookRotation = Quaternion.LookRotation(_direction);
+
+            //transform.LookAt(targetTransform);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetTransform, step);
+            print(Quaternion.Inverse(_lookRotation) * transform.rotation);
+            if (differenceOfRotation(_lookRotation, transform.rotation) > 0.2)
+                transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * rotationSpeed);
+            else {
+
+                targetX = Random.Range(targetX - 1, targetX + 1);
+                targetY = Random.Range(targetY - 1, targetY + 1);
+                targetZ = Random.Range(targetZ - 1, targetZ + 1);
+                lookTransform = new Vector3(targetX, targetY, targetZ);
+                _direction = (lookTransform - transform.position).normalized;
+                _lookRotation = Quaternion.LookRotation(_direction);
+            }
+
+        }
+    }
+
+    
 }
