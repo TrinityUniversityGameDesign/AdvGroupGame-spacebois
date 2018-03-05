5f4d6964aa3727307640de75bcb98341d3a4c82d
Nick Samoray <nicksam112@gmail.com>
2018-03-04 18:28:29 -0600

    Cleanup and Perlin Noise

---
+++ Assets/Scripts/PerlinMeshModifier.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinMeshModifier : MonoBehaviour {
+    Mesh mesh;
+    Vector3[] verts;
+    // Use this for initialization
+    void Start () {
+        mesh = GetComponent<MeshFilter>().mesh;
+        verts = mesh.vertices;
+        Vector3[] newVerts = new Vector3[verts.Length];
+        int i = 0;
+        foreach(Vector3 v in verts)
+        {
+            Vector3 vertPos = transform.TransformPoint(v);
+            //Debug.Log(vertPos);
+            newVerts[i] = vertPos + Vector3.one * Mathf.PerlinNoise(vertPos.x, vertPos.y);
+            i++;
+        }
+        mesh.vertices = newVerts;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        gameObject.AddComponent<SphereCollider>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
