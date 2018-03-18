73bec084aed638ea6d1c304db4d22ce33e8c62dc
Brent Oakes <boakes1@trinity.edu>
2018-03-18 16:59:01 -0500

    changed tagging

--- Assets/Scripts/playerDeath.cs
+++ Assets/Scripts/playerDeath.cs
@@ -8,16 +8,15 @@ public class playerDeath : MonoBehaviour {
 	// Use this for initialization
 	public void die(){
 		//if we aren't the master client
+		this.gameObject.tag = "Dead";
 		if (!PhotonNetwork.isMasterClient)
         {
-          this.gameObject.tag = "Dead";
    		  SceneManager.LoadScene("DeadTest");
         }
         else {
         	//Maybe have a call to give the session to a new player? 
         	Debug.LogWarning("MASTER CLIENT PLAYER CALLING DEAD");
-        	this.gameObject.tag = "Dead";
-			//SceneManager.LoadScene("DeadTest");
+        	//SceneManager.LoadScene("DeadTest");
         }
 	}
 
