1095296311cab9ebfbf82e1de38f77ec039a489d
Nick Samoray <nicksam112@gmail.com>
2018-04-28 18:50:16 -0500

    End Scene

---
+++ Assets/Scripts/ScoreContainer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class ScoreContainer {
+    private static PhotonPlayer[] pList;
+    public static PhotonPlayer[] scores
+    {
+        get
+        {
+            return pList;
+        }
+        set
+        {
+            pList = value;
+        }
+    }
+}
